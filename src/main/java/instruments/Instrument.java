b9c2045a9266bde91aa6f2e908c01529dfa7ef9a
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-09-04 21:17:43 +0100

    2nd commit instrument class complete

--- src/main/java/instruments/Instrument.java
+++ src/main/java/instruments/Instrument.java
@@ -1,4 +1,68 @@
 package instruments;
 
 public class Instrument {
+
+    private String manufacturer;
+    private String type;
+    private String colour;
+    private String material;
+    private int purchasePrice;
+    private int sellingPrice;
+
+    public Instrument(String manufacturer, String type, String colour, String material, int purchasePrice, int sellingPrice){
+        this.manufacturer = manufacturer;
+        this.type = type;
+        this.colour = colour;
+        this.material = material;
+        this.purchasePrice = purchasePrice;
+        this.sellingPrice = sellingPrice;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public int getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(int purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public int getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(int sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
 }
