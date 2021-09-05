6ed8d657270c92bfa6f1ba45ffebd9f4e72de365
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-09-05 22:08:42 +0100

    oboe accessories class and tests complete

--- src/main/java/accessories/OboeCase.java
+++ src/main/java/accessories/OboeCase.java
@@ -2,5 +2,54 @@ package accessories;
 
 import behaviours.ISell;
 
-//public class OboeCase implements ISell {
-//}
+public class OboeCase implements ISell {
+
+    private String brand;
+    private String description;
+    private double purchasePrice;
+    private double sellingPrice;
+
+    public OboeCase(String brand, String description, double purchasePrice, double sellingPrice) {
+        this.brand = brand;
+        this.description = description;
+        this.purchasePrice = purchasePrice;
+        this.sellingPrice = sellingPrice;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(double sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    @Override
+    public double calculateMarkup() {
+        return this.getSellingPrice() - getPurchasePrice();
+    }
+}
--- src/test/java/OboeCaseTest.java
+++ src/test/java/OboeCaseTest.java
@@ -1,2 +1,41 @@
+import accessories.OboeCase;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class OboeCaseTest {
+
+    OboeCase oboeCase;
+
+    @Before
+    public void before(){
+        oboeCase = new OboeCase("SKB", "Lightweight Oboe Case", 55, 75);
+    }
+
+    @Test
+    public void accessoriesHaveBrand(){
+        assertEquals("SKB", oboeCase.getBrand());
+    }
+
+    @Test
+    public void accessoriesHaveDescription(){
+        assertEquals("Lightweight Oboe Case", oboeCase.getDescription());
+    }
+
+    @Test
+    public void accessoriesHavePurchasePrice(){
+        assertEquals(55, oboeCase.getPurchasePrice(), 0.01);
+    }
+
+    @Test
+    public void accessoriesHaveSellingPrice(){
+        assertEquals(75, oboeCase.getSellingPrice(), 0.01);
+    }
+
+    @Test
+    public void canCalculateMarkupValue(){
+        assertEquals(20, oboeCase.calculateMarkup(), 0.01);
+    }
+
 }
